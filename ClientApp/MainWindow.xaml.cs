bd005d60b9217c8ec5374640efcb5a553e3b964f
Maksim_Gladkov <60353057+Hokage-git@users.noreply.github.com>
2022-04-10 12:03:54 +0500

    Добавьте файлы проекта.

---
+++ ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace ClientApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private const string host = "127.0.0.1";
+        private const int port = 1337;
+        public static TcpClient client;
+        public static NetworkStream stream;
+        static string userName;
+        public static bool checkmes = false;
+        static public Window1 mc = new Window1();
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            client = new TcpClient();
+            if (NameStroke.Text == "")
+            {
+                return;
+            }
+            else
+            {
+                userName = NameStroke.Text;
+            }
+            try
+            {
+            client.Connect(host, port);
+            stream = client.GetStream();
+
+            string message = userName;
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+
+            // запускаем новый поток для получения данных
+            Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+            receiveThread.Start(); //старт потока
+            SendMessage();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void SendMessage()
+        {
+            Window1 win = new Window1();
+            win.vvod.Text=("Введите сообщение: ");
+            this.Close();
+            win.Show();
+            mc.Chats.Items.Add(("Добро пожаловать, {0}", userName));
+        }
+
+        static void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    mc.Chats.Items.Add(message);//вывод сообщения
+                }
+                catch
+                {
+                    Console.WriteLine("Подключение прервано!"); //соединение было прервано
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();//отключение потока
+            if (client != null)
+                client.Close();//отключение клиента
+            Environment.Exit(0); //завершение процесса
+        }
+    }
+}
---
+++ ClientApp/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace ClientApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message;
+            message = vvod.Text;
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            MainWindow.stream.Write(data, 0, data.Length);
+        }
+
+        private void DataWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (MainWindow.client != null)
+                MainWindow.client.Close();//отключение клиента
+            if (MainWindow.stream != null)
+                MainWindow.stream.Close();//отключение потока
+            Environment.Exit(0); //завершение процесса
+        }
+    }
+}
